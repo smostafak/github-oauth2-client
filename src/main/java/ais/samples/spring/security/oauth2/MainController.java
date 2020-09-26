31c5e50f034067ade1c9aad442c0b0ea422a01e2
root <acefgb#1394>
2020-09-26 13:18:57 +0330

    log the details of the logged-in user

--- src/main/java/ais/samples/spring/security/oauth2/MainController.java
+++ src/main/java/ais/samples/spring/security/oauth2/MainController.java
@@ -1,5 +1,8 @@
 package ais.samples.spring.security.oauth2;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -10,9 +13,11 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class MainController {
+  private static final Logger log = LoggerFactory.getLogger(MainController.class);
 
   @GetMapping("/")
-  public String main() {
+  public String main(OAuth2AuthenticationToken token) {
+    log.info(String.valueOf(token.getPrincipal()));
     return "main.html";
   }
 }
